9f0877ff42d6fa056bfa0b556cf18287e0505d85
Avinashpalofficial <apal73451@gmail.com>
2025-07-31 00:36:58 +0530

    set the email and password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,32 @@
+import React, { useState } from 'react';
 function Login() {
+       const [email, setEmail] = useState("");
+       const [password, setPassword] = useState("");
+    const handleLogin = (e) => {
+       e.preventDefault();
+       console.log("Login submitted");
+       console.log("Email:", email);
+       console.log("Password:", password);
+    }
   return (
     <div>
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-2xl font-bold">Admin Panel</h1>
-            <form action="" className="flex flex-col gap-4 bg-white p-6 rounded shadow-md w-80">
+            <form onSubmit={handleLogin} className="flex flex-col gap-4 bg-white p-6 rounded shadow-md w-80">
                 <div className="mb-4">
                     <label className="block text-sm font-medium mb-1">Email</label>
-                    <input type="text" placeholder="Email" className="border p-2 rounded w-full" />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" placeholder="Email" className="border p-2 rounded w-full" />
                 </div>
                 <div className="mb-4">
                     <label className="block text-sm font-medium mb-1">Password</label>
-                    <input type="password" placeholder="Password" className="border p-2 rounded w-full" />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder="Password" className="border p-2 rounded w-full" />
                 </div>
                 <button type="submit" className="bg-blue-500 text-white p-2 rounded">Login</button>
             </form>
         </div>
     </div>
   )
+
 }
 
 export default Login;
