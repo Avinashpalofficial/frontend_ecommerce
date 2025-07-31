9835a41e1069c1b45051f3d754ec71295bfb1b39
Avinashpalofficial <apal73451@gmail.com>
2025-07-31 12:16:56 +0530

    create a upload area

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,8 +1,57 @@
+import { assets } from "../assets/assets"
+
 function Add() {
+  
+
   return (
-    <div>
-      <h1>Add Item</h1>
-    </div>
+         <form >
+           <p>Upload Image</p>
+         <div className="flex gap-2" >
+         
+        <div className=" ">
+          <label htmlFor="image1" className=" mb-1 font-semibold">
+          <input
+            type="file"
+            id="image1"
+            hidden
+          />
+          <img src={assets.upload_area} alt="" className="w-20" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="image2" className=" mb-1 font-semibold">
+          <input
+            type="file"
+            id="image2"
+            hidden
+          />
+          <img src={assets.upload_area} alt="" className="w-20" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="image3" className=" mb-1 font-semibold">
+          <input
+            type="file"
+            id="image3"
+            hidden
+          />
+          <img src={assets.upload_area} alt="" className="w-20" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="image4" className=" mb-1 font-semibold">
+          <input
+            type="file"
+            id="image4"
+            hidden
+          />
+          <img src={assets.upload_area} alt="" className="w-20" />
+          </label>
+        </div>
+         </div>
+       </form>
+       
+   
   )
 }
 
